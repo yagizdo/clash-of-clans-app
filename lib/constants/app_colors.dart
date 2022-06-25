0833bc78a16abe39a392bbb2fa9427191a9fc271
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-25 16:25:52 +0300

    updated background color

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 const black = Color(0xFF101823);
+const scaffoldBackground = Color(0xFFEFE1C4);
 const black_second = Colors.black;
 const white = Color(0xFFFFFFFF);
 const grey = Color(0xFF787878);
@@ -10,9 +11,9 @@ const grey = Color(0xFF787878);
 class AppTheme {
   final appTheme = ThemeData(
     // Scaffold
-    scaffoldBackgroundColor: black,
+    scaffoldBackgroundColor: scaffoldBackground,
     // Appbar
-    appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent,elevation: 0),
+    appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent,elevation: 0.1),
     // Text
     textTheme: TextTheme(
       headline1: TextStyle(
