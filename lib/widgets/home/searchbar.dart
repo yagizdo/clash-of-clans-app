a3d488257ec0bd3c1591719c966be38d721e61e8
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-25 16:45:19 +0300

    added controller

--- lib/widgets/home/searchbar.dart
+++ lib/widgets/home/searchbar.dart
@@ -9,11 +9,22 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    controller = TextEditingController();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.w),
       child: TextField(
+        controller: controller,
+        onChanged: (value) {
+
+        },
         decoration: InputDecoration(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20.w),
