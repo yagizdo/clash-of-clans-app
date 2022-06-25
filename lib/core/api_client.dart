d316ffe61ca056ffcb705e19eb22901372f2eebd
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-25 21:46:42 +0300

    dotenv init

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiClient {
   // https://api.clashofclans.com/v1/players/
   final baseUrl = 'https://api.clashofclans.com/';
+  final apiKey = dotenv.get('API_KEY');
 
   Dio dio = Dio();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
