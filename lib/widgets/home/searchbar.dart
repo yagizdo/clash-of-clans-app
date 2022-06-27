e959c7baddd482fa25ef4f6c30fade71462207ad
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-27 22:19:47 +0300

    added error system

--- lib/widgets/home/searchbar.dart
+++ lib/widgets/home/searchbar.dart
@@ -12,9 +12,8 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
-
   // Text controller for the search bar
-  late TextEditingController controller;
+  final TextEditingController _controller = TextEditingController();
 
   // Debouncer for the search bar
   Debouncer debouncer = Debouncer(milliseconds: 500);
@@ -22,23 +21,52 @@ class _SearchBarState extends State<SearchBar> {
 // Profile client for the search bar
   ProfileClient profileClient = ProfileClient();
 
+  // focus node for the search bar
+  FocusNode _focusNode = FocusNode();
+
+  // Text
+  String _valueText = '';
 
   @override
   void initState() {
-    controller = TextEditingController();
     super.initState();
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  String? get _errorText {
+    if (_valueText.isEmpty && _focusNode.hasFocus) {
+      return 'Cant\'t be empty';
+    }
+    if (_valueText.length < 6 && _focusNode.hasFocus) {
+      return 'Must be at least 6 characters';
+    }
+    // If no error, return null
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.w),
       child: TextField(
-        controller: controller,
+        // Controller for the search bar
+        controller: _controller,
+
+        // Focus node for the search bar
+        focusNode: _focusNode,
+
+        // On changed for the search bar
         onChanged: (value) {
-          if(value != '') {
-            if(value.contains('#')) {
-             print('YOU BREAK THE RULE GOD DAMN IT');
-            } else {
+          setState(() {
+            // Update the value of the text
+            _valueText = value.toString();
+
+            if (_errorText == null) {
               // Run the debouncer
               debouncer.run(() {
                 profileClient.getPlayer(value).then((value) {
@@ -46,9 +74,12 @@ class _SearchBarState extends State<SearchBar> {
                 });
               });
             }
-          }
+          });
         },
+
+        // Decoration for the search bar
         decoration: InputDecoration(
+          errorText: _errorText,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20.w),
           ),
