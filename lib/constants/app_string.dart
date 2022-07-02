bc7f117144ec4d341ad5f2941543e899319896d3
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-02 22:08:00 +0300

    added images and images strings

--- lib/constants/app_string.dart
+++ lib/constants/app_string.dart
@@ -2,11 +2,81 @@ const homeTitle = 'Home';
 const appName = 'Clash of Clans App';
 
 // Heroes
-const barbarianKing = 'lib/img/barbarian_king.webp';
-const archerQueen = 'lib/img/archer_queen.webp';
-const battleMachine = 'lib/img/battle_machine.webp';
-const grandWarden = 'lib/img/grand_warden.webp';
-const royalChampion = 'lib/img/royal_champion.webp';
+const barbarianKing = 'lib/img/heroes/barbarian_king.webp';
+const archerQueen = 'lib/img/heroes/archer_queen.webp';
+const battleMachine = 'lib/img/heroes/battle_machine.webp';
+const grandWarden = 'lib/img/heroes/grand_warden.webp';
+const royalChampion = 'lib/img/heroes/royal_champion.webp';
+
+// Elixir Troops
+const barbarian = 'lib/img/troops/Barbarian.webp';
+const archer = 'lib/img/troops/Archer.webp';
+const giant = 'lib/img/troops/Giant.webp';
+const goblin = 'lib/img/troops/Goblin.webp';
+const wall_breaker = 'lib/img/troops/Wall_Breaker.webp';
+const balloon = 'lib/img/troops/Balloon.webp';
+const wizard = 'lib/img/troops/Wizard.webp';
+const healer = 'lib/img/troops/Healer.webp';
+const dragon = 'lib/img/troops/Dragon.webp';
+const pekka = 'lib/img/troops/Pekka.webp';
+const baby_dragon = 'lib/img/troops/Baby_Dragon.webp';
+const miner = 'lib/img/troops/Miner.webp';
+const electro_dragon = 'lib/img/troops/Electro_Dragon.webp';
+const yeti = 'lib/img/troops/Yeti.webp';
+const dragon_rider = 'lib/img/troops/Dragon_Rider.webp';
+
+// Dark Elixir Troops
+const minion = 'lib/img/troops/Minion.webp';
+const hog_rider = 'lib/img/troops/Hog_Rider.webp';
+const valkyrie = 'lib/img/troops/Valkyrie.webp';
+const golem = 'lib/img/troops/Golem.webp';
+const lava_hound = 'lib/img/troops/Lava_Hound.webp';
+const bowler = 'lib/img/troops/Bowler.webp';
+const ice_golem = 'lib/img/troops/Ice_Golem.webp';
+const head_hunter = 'lib/img/troops/Headhunter.webp';
+
+// Super Troops
+const super_barbarian = 'lib/img/troops/Super_Barbarian.webp';
+const super_archer = 'lib/img/troops/Super_Archer.webp';
+const super_giant = 'lib/img/troops/Super_Giant.webp';
+const sneaky_goblin = 'lib/img/troops/Sneaky_Goblin.webp';
+const super_wall_breaker = 'lib/img/troops/Super_Wall_Breaker.webp';
+const rocket_balloon = 'lib/img/troops/Rocket_Balloon.webp';
+const super_wizard = 'lib/img/troops/Super_Wizard.webp';
+const super_dragon = 'lib/img/troops/Super_Dragon.webp';
+const inferno_dragon = 'lib/img/troops/Inferno_Dragon.webp';
+const super_minion = 'lib/img/troops/Super_Minion.webp';
+const super_valkyrie = 'lib/img/troops/Super_Valkyrie.webp';
+const super_witch = 'lib/img/troops/Super_Witch.webp';
+const ice_hount = 'lib/img/troops/Ice_Hount.webp';
+const super_bowler = 'lib/img/troops/Super_Bowler.webp';
+
+// Builder Base Troops
+const raged_barbarian = 'lib/img/troops/Raged_Barbarian.webp';
+const sneaky_archer = 'lib/img/troops/Sneaky_Archer.webp';
+const boxer_giant = 'lib/img/troops/Boxer_Giant.webp';
+const beta_minion = 'lib/img/troops/Beta_Minion.webp';
+const bomber = 'lib/img/troops/Bomber.webp';
+const cannon_cart = 'lib/img/troops/Cannon_Cart.webp';
+const night_witch = 'lib/img/troops/Night_Witch.webp';
+const drop_ship = 'lib/img/troops/Drop_Ship.webp';
+const super_pekka = 'lib/img/troops/Super_Pekka.webp';
+const hog_glider = 'lib/img/troops/Hog_Glider.webp';
+
+// Spells
+const lightning_spell = 'lib/img/spells/Lightning_Spell.webp';
+const healing_spell = 'lib/img/spells/Healing_Spell.webp';
+const freeze_spell = 'lib/img/spells/Freeze_Spell.webp';
+const poison_spell = 'lib/img/spells/Poison_Spell.webp';
+const haste_spell = 'lib/img/spells/Haste_Spell.webp';
+const bat_spell = 'lib/img/spells/Bat_Spell.webp';
+const skeleton_spell = 'lib/img/spells/Skeleton_Spell.webp';
+const clone_spell = 'lib/img/spells/Clone_Spell.webp';
+const earthquake_spell = 'lib/img/spells/Earthquake_Spell.webp';
+const invisibility_spell = 'lib/img/spells/Invisibility_Spell.webp';
+const jump_spell = 'lib/img/spells/Jump_Spell.webp';
+const rage_spell = 'lib/img/spells/Rage_Spell.webp';
+
 
 // Level Star
 const levelStar = 'lib/img/level_star.png';
